f18fa57839af41c195725cb6b589934118c4e3f4
Mike Solomon <mike@mikesolomon.org>
2018-12-28 05:06:06 +0200

    Basic example with unmock and mock-mongo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,36 @@
 import React from 'react'
-import Link from 'next/link'
 
-export default () => (
-  <ul>
-    <li><Link href='/a' as='/a'><a>a</a></Link></li>
-    <li><Link href='/b' as='/b'><a>b</a></Link></li>
-  </ul>
-)
+export default class EmailForm extends React.Component<{
+
+}, {
+  value: string;
+}> {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    window.location.href = `/porterduff?email=${this.state.value}`;
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          E-mail:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,19 @@
+import axios from 'axios';
+import * as mongodb from 'mongo-mock';
 import { createServer } from 'http'
 import { parse } from 'url'
 import * as next from 'next'
+
+const unmockify = async () => {
+  if (process.env.NODE_ENV !== "production") {
+    const { unmock } = require("unmock");
+    await unmock({ ignore: "story "});
+  }
+}
+
+unmockify();
+
+const MongoClient = mongodb.MongoClient;
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
@@ -9,14 +22,37 @@ const handle = app.getRequestHandler()
 
 app.prepare()
 .then(() => {
-  createServer((req, res) => {
+  createServer(async (req, res) => {
     const parsedUrl = parse(req.url, true)
     const { pathname, query } = parsedUrl
 
-    if (pathname === '/a') {
-      app.render(req, res, '/a', query)
-    } else if (pathname === '/b') {
-      app.render(req, res, '/b', query)
+    if (pathname === '/porterduff') {
+      const url = 'mongodb://localhost:27017/myproject';
+      const db = await MongoClient.connect(url);
+      const collection = db.collection('users');
+      const { email } = query;
+      const findResult = await collection.find({ email });
+      const findArray = await findResult.toArray();
+      if (findArray.length === 0) {
+        console.log('now inserting email');
+        collection.insert({ email });
+        await axios.post("https://api.sendgrid.com/v3/mail/send", {
+          personalizations: [{
+            to: {
+              email,
+            }
+          }],
+          subject: "hi there!",
+          from: { email: "mike@porterduff.io", name: "Mike Solomon"}
+        }, {
+          headers: {
+            Authorization: `Bearer u_n_m_o_c_k_200` // change to process.env
+          }
+        });
+      } else {
+        console.log('email already inserted - skipping insert');
+      }
+      app.render(req, res, '/porterduff')
     } else {
       handle(req, res, parsedUrl)
     }
