209afe6dd430d73fe067fbe7ae8ba7efe9af6ac8
Mike Solomon <mike@mikesolomon.org>
2019-02-07 18:20:13 -0800

    Fixes test

--- test/server/index.test.ts
+++ test/server/index.test.ts
@@ -1,7 +1,7 @@
 import { unmock } from "unmock";
 import { postEmailToSendgrid } from "../../server/util";
 
-beforeEach(async () => await unmock());
+beforeEach(async () => await unmock({ token: process.env.UNMOCK_TOKEN }));
 
 test("will fail, need to fix with unmock", async () => {
     await postEmailToSendgrid("mike@meeshkan.com");
